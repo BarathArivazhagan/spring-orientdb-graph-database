0cd8a86afbe396ab551cabe82cd64bb93bf967d3
BarathArivazhagan <barathmacdec91@gmail.com>
2019-01-17 21:26:54 +0530

    added test case

--- src/test/java/com/barath/app/OrientDBGraphTest.java
+++ src/test/java/com/barath/app/OrientDBGraphTest.java
@@ -9,6 +9,7 @@ import com.tinkerpop.blueprints.Vertex;
 import com.tinkerpop.blueprints.impls.orient.OrientEdge;
 import com.tinkerpop.blueprints.impls.orient.OrientGraph;
 import com.tinkerpop.blueprints.impls.orient.OrientGraphFactory;
+import com.tinkerpop.blueprints.impls.orient.OrientGraphNoTx;
 
 public class OrientDBGraphTest {
 	
@@ -19,7 +20,7 @@ public class OrientDBGraphTest {
 	
 	
 	@Test
-	public void testOrientDB() {
+	public void testOrientDBWithTx() {
 		
 		OrientGraphFactory graphFactory = new OrientGraphFactory(ORIENTDB_URI,ORIENTDB_USERNAME,ORIENTDB_PASSWORD);
 		OrientGraph graph=graphFactory.getTx();
@@ -43,6 +44,51 @@ public class OrientDBGraphTest {
 			logger.info("EDGE label {}",edge.getLabel());
 		});
 		
+	}
+	
+	@Test
+	public void testOrientDBWithNoTx() {
+		
+		OrientGraphFactory graphFactory = new OrientGraphFactory(ORIENTDB_URI,ORIENTDB_USERNAME,ORIENTDB_PASSWORD);
+		OrientGraphNoTx graph=graphFactory.getNoTx();
+		
+		Vertex person1=	graph.addVertex("class:Person");
+		person1.setProperty("name", "PANKAJ");
+		person1.setProperty("age", 26);
+		
+		Vertex vAddress = graph.addVertex("class:Address");
+		vAddress.setProperty("street", "Van Ness Ave.");
+		vAddress.setProperty("city", "San Francisco");
+		vAddress.setProperty("state", "California");
+		
+		OrientEdge livesEdge=graph.addEdge(null, person1, vAddress, "lives");
+		System.out.println("Edge is created "+livesEdge);
+	
+		graph.commit();
+		Iterable<Edge>  edges=graph.getEdgesOfClass("lives");
+		edges.forEach( (edge) -> {
+			edge.getPropertyKeys().forEach( key -> logger.info(" key {}",key));
+			logger.info("EDGE label {}",edge.getLabel());
+		});
+		
+	}
+	
+	@Test
+	public void testOrientDBOrientGraph() {
+		OrientGraph graph = new OrientGraph(ORIENTDB_URI,ORIENTDB_USERNAME, ORIENTDB_PASSWORD);
+        try {
+            Vertex vPerson = graph.addVertex("class:Person");
+            vPerson.setProperty("name", "John");
+            vPerson.setProperty("age", 24);
+
+            Vertex vAddress = graph.addVertex("class:Address");
+            vAddress.setProperty("street", "Van Ness Ave.");
+            vAddress.setProperty("city", "San Francisco");
+            vAddress.setProperty("state", "California");
+        } finally {
+            graph.shutdown();
+        }   
+       
 	}
 
 }
